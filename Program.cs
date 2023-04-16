4f7efcd45e87bdffdba8ad1aaeb6ebdb5ec0a7ed
José victor andrade oliveira <victorandrade150@gmail.com>
2023-04-16 13:35:54 -0300

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,7 @@
+﻿using Azure.Messaging.ServiceBus;
+
+// connection string to your Service Bus namespace
+string connectionString = "Endpoint=sb://servicebuslab305.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=IoNrvUVS8NgGNlguf0bipK4dVc/aeqpPR+ASbGXG9sw=";
+
+// name of your Service Bus topic
+string queueName = "primeira-queue";
