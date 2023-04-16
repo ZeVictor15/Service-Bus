b184982c179f34d666172347b47299550977f384
José victor andrade oliveira <victorandrade150@gmail.com>
2023-04-16 14:11:38 -0300

    update

--- Program.cs
+++ Program.cs
@@ -1,7 +1,77 @@
 ﻿using Azure.Messaging.ServiceBus;
 
-// connection string to your Service Bus namespace
 string connectionString = "Endpoint=sb://servicebuslab305.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=IoNrvUVS8NgGNlguf0bipK4dVc/aeqpPR+ASbGXG9sw=";
 
-// name of your Service Bus topic
 string queueName = "primeira-queue";
+
+ServiceBusClient client;
+
+ServiceBusSender sender;
+
+client = new ServiceBusClient(connectionString);
+sender = client.CreateSender(queueName);
+
+using ServiceBusMessageBatch messageBatch = await sender.CreateMessageBatchAsync();
+
+for (int i = 1; i <= 3; i++)
+{
+    if (!messageBatch.TryAddMessage(new ServiceBusMessage($"Message {i}")))
+    {
+        throw new Exception($"Exception {i} has occurred.");
+    }
+}
+
+try
+{
+    await sender.SendMessagesAsync(messageBatch);
+    Console.WriteLine($"A batch of three messages has been published to the queue.");
+}
+finally
+{
+    await sender.DisposeAsync();
+    await client.DisposeAsync();
+}
+
+Console.WriteLine("Follow the directions in the exercise to review the results in the Azure portal.");
+Console.WriteLine("Press any key to continue");
+Console.ReadKey();
+
+ServiceBusProcessor processor;
+client = new ServiceBusClient(connectionString);
+
+processor = client.CreateProcessor(queueName, new ServiceBusProcessorOptions());
+
+try
+{
+    processor.ProcessMessageAsync += MessageHandler;
+
+    processor.ProcessErrorAsync += ErrorHandler;
+
+    await processor.StartProcessingAsync();
+
+    Console.WriteLine("Wait for a minute and then press any key to end the processing");
+    Console.ReadKey();
+
+    Console.WriteLine("\nStopping the receiver...");
+    await processor.StopProcessingAsync();
+    Console.WriteLine("Stopped receiving messages");
+}
+finally
+{
+    await processor.DisposeAsync();
+    await client.DisposeAsync();
+}
+
+async Task MessageHandler(ProcessMessageEventArgs args)
+{
+    string body = args.Message.Body.ToString();
+    Console.WriteLine($"Received: {body}");
+
+    await args.CompleteMessageAsync(args.Message);
+}
+
+Task ErrorHandler(ProcessErrorEventArgs args)
+{
+    Console.WriteLine(args.Exception.ToString());
+    return Task.CompletedTask;
+}
